931eba7272f1367393e0348506b183a970006fa1
Yong Wen Chua <me@yongwen.xyz>
2017-02-24 12:51:15 +0800

    Add klokantech_basic

--- copy-style-json.sh
+++ copy-style-json.sh
@@ -7,6 +7,7 @@ declare -A STYLES=( \
                     ["positron"]="styles/positron-gl-style/style.json" \
                     ["dark_matter"]="styles/dark-matter-gl-style/style.json" \
                     ["osm_bright"]="styles/osm-bright-gl-style/style.json" \
+                    ["klokantech_basic"]="styles/klokantech-basic-gl-style/style.json" \
                  )
 mkdir -p "${STYLES_DIR}"
 
